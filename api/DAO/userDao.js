f88dd745fef0efea3ed7915988a2e95dd648af5a
Danillo Romano <danilloferomano@gmail.com>
2017-05-30 11:26:29 -0300

    corrigido layout de usuarios e parte get e post funcionando

--- api/DAO/userDao.js
+++ api/DAO/userDao.js
@@ -3,42 +3,17 @@ function userDao(connection) {
 };
 
 userDao.prototype.saveUser = function(novoUser,callback) {
-    this._connection.query('INSERT INTO users SET ?',novoUser, callback);
+    this._connection.query('INSERT INTO adminUsers SET ?',novoUser, callback);
     console.log(this._connection.query);
-}
-
-userDao.prototype.saveUserAddress = function(newUserAddress,callback) {
-    this._connection.query('INSERT INTO address SET ?', newUserAddress, callback);
-}
-
-// userDao.prototype.list = function(user,callback) {
-//     this._connection.query("select a.*,u.*,r.* from users as u join address as a on u.rg = a.user_rg join roles as r on r.id = u.user_role;"
-// , user, callback);
-// }
+};
 
 userDao.prototype.list = function(user,callback) {
     this._connection.query("select * from adminUsers;", user, callback);
-}
+};
 
-userDao.prototype.changeUser = function(user,rg,callback){
-  this._connection.query("UPDATE users set ? WHERE rg = ? ",[user,rg],callback);
-}
-
-userDao.prototype.changeAddress = function(user,address,rg,callback){
-  this._connection.query("UPDATE address set ? WHERE rg = ? ",[address,rg],callback);
-}
-
-// userDao.prototype.alteraProduto = function(user,callback) {
-//     this._connection.query("update produtos set nome = user.name",)
-// }
-
-userDao.prototype.categoryUser = function(role,callback){
-  this._connection.query("select * from roles",role,callback);
-}
-
-userDao.prototype.searchForId = function (id,callback) {
-    this._connection.query("select * from pagamentos where id = ?",[id],callback);
-}
+userDao.prototype.changeUser = function(user,id,callback){
+  this._connection.query("UPDATE adminUsers set ? WHERE id = ? ",[user,id],callback);
+};
 
 module.exports = function(){
     return userDao;
--- public/js/controllers/user-controller.js
+++ public/js/controllers/user-controller.js
@@ -2,7 +2,6 @@ angular.module('impulseApp').controller('UserController',function($scope,$http,$
 
   $scope.users = [];
   $scope.novoUser = {};
-  $scope.roles = [];
   $scope.newUserAddress = {};
 	$scope.filtro = '';
   $scope.showUpdateUserModal = false;
@@ -18,7 +17,7 @@ angular.module('impulseApp').controller('UserController',function($scope,$http,$
     console.log("funcionei");
     console.log(valorX);
     return objeto;
-  }
+  };
 
   var promise = $http.get('/usersData');
   promise.then(function(result){
@@ -27,39 +26,32 @@ angular.module('impulseApp').controller('UserController',function($scope,$http,$
     console.log(error);
   });
 
-  var promise = $http.get('/user_role');
-  promise.then(function(result){
-      $scope.roles = result.data;
-  }).catch(function(error){
-    console.log(error);
-  });
-
-  $scope.updateRole = function(role){
-    $scope.roleInEdition = role;
-  }
+  // $scope.updateRole = function(role){
+  //   $scope.roleInEdition = role;
+  // }
 
   $scope.updateUser = function(user){
     $scope.showUpdateUserModal = true;
     $scope.isOpen = true;
     $scope.userInEdition = user;
-  }
+  };
 
-    $scope.closeModal = function($event,element){
+    $scope.closeModal = function($event,element) {
     $scope.showUpdateUserModal = false;
-  }
+    };
 
   $scope.newUser = function(){
     $scope.isOpen = true;
     $scope.showNewUserModal = true;
-  }
+  };
 
   $scope.close = function(){
     $scope.novoUser = "";
     $scope.isOpen = false;
-    setTimeout( function() {
+    setTimeout(function() {
       $scope.showNewUserModal = false;
     }, 500)
-  }
+  };
 
   $scope.save = function(novoUser){
     console.log($scope.novoUser);
@@ -67,83 +59,36 @@ angular.module('impulseApp').controller('UserController',function($scope,$http,$
       first_name: novoUser.first_name,
       last_name: novoUser.last_name,
       email: novoUser.email,
-      cpf: novoUser.cpf,
-      rg: novoUser.rg,
       password: novoUser.password,
-      phone: novoUser.phone,
-      user_role: novoUser.user_role,
       active: pegaCheck(),
-      birth:'2017/12/12'
     };
+
     console.log($scope.userInSave);
     var promise = $http.post('/users/newUser', $scope.userInSave);
     promise.then(function(){
-    $scope.userInSave = {};
-    console.log($scope.userInSave);
+      $scope.users.push($scope.userInSave);
     }).catch(function(error){
       console.log(error);
     });
-  }
+  };
 
-  $scope.saveUserAddress = function(novoUser){
-      $scope.newUserAddress= {
-      address_name :novoUser.address_name,
-      neighborhood : novoUser.neighborhood,
-      number : novoUser.number,
-      cep : novoUser.cep,
-      addition : novoUser.addition,
-      user_rg  : novoUser.rg
-    };
-
-    console.log($scope.newUserAddress);
-    var promise = $http.post('/users/newUser/address',$scope.newUserAddress);
-    promise.then(function(){
-      $scope.newUserAddress = {};
-      console.log($scope.newUserAddress);
-    }).catch(function(error){
-      console.log(error);
-    });
-  }
-
-  $scope.changeUser = function(userInEdition){
+  $scope.changeUser = function(novoUser){
+    console.log(novoUser);
     $scope.userInChange = {
-      first_name: userInEdition.first_name,
-      last_name: userInEdition.last_name,
-      email: userInEdition.email,
-      cpf: userInEdition.cpf,
-      rg: userInEdition.rg,
-      password: userInEdition.password,
-      phone: userInEdition.phone,
-      user_role: userInEdition.user_role,
-      active: userInEdition.active,
-      birth:'2017/12/12'
+      first_name: novoUser.first_name,
+      last_name: novoUser.last_name,
+      email: novoUser.email,
+      password: novoUser.password,
+      active: novoUser.active,
     };
 
-    var promise = $http.put('/users/user/change',$scope.userInChange,$scope.userInChange.rg);
+    var promise = $http.put('/users/user/change',$scope.userInChange,$scope.userInChange.id);
     promise.then(function(){
       $scope.userInChange = {};
       console.log($scope.userInChange);
     }).catch(function(error){
       console.log(error);
     });
-  }
+  };
 
-  $scope.changeAddress = function(userInEdition){
-    $scope.addressInChange = {
-      address_name :userInEdition.address_name,
-      neighborhood : userInEdition.neighborhood,
-      number : userInEdition.number,
-      cep : userInEdition.cep,
-      addition : userInEdition.addition,
-      user_rg  : userInEdition.rg
-    };
-
-    var promise = $http.put('/users/user/change',$scope.addressInChange,$scope.addressInChange.user_rg);
-    promise.then(function(){
-      $scope.addressInChange = {};
-      console.log($scope.addressInChange);
-    }).catch(function(error){
-      console.log(error);
-    });
-  }
 });
