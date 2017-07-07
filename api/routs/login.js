1219d5ce2486398dcbe3ca3e693849998dfea357
Danillo Romano <danilloferomano@gmail.com>
2017-07-07 13:20:41 -0300

    corrigido problema de crash no login

--- api/routs/login.js
+++ api/routs/login.js
@@ -15,29 +15,23 @@ module.exports = function(app) {
     let loginDao =  new app.DAO.loginDao(connection);
 
     loginDao.searchUser(params,function(error,result) {
-      console.log(result);
-
-      if (error) {
-        console.log(error);
-        res.status(500).send(error);
-      }
-
-      let vazio = [];
 
       if (result.length === 0) {
         let algumaCoisa = {email:"a",password:"a"};
         result.push(algumaCoisa);
-        res.status(401).send(error);
+        res.status(404).send(error);
         console.log("erro encontrado no login");
       }
 
       if(params[0] === result[0].email && params[1] === result[0].password ) {
           console.log("usuario logado");
           res.redirect("/#!/home");
+          return res.status(200);
       } else  {
           console.log("Erro ao logar, usuario ou senha invalida");
-          res.status(500).send(error);
+          params = [];
           connection.end();
+          return res.status(500);
         };
     });
   });
