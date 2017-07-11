5f8991c36c5e7d31cb42cd6db081dd49f6097e8f
Danillo Romano <danilloferomano@gmail.com>
2017-07-11 10:35:19 -0300

    tentando resolver problema de conexao com o banco

--- api/DAO/connection.js
+++ api/DAO/connection.js
@@ -6,19 +6,21 @@ function createDBConnection(){
   if(!process.env.NODE_ENV) {
         return mysql.createConnection({
                 host:'localhost',
-                user:'danillo',
+                user:'root',
                 password:'dfromano1',
-                database:'immpulse_php'
+                database:'impulse_php'
         });
     }
+
     if (process.env.NODE_ENV == 'production') {
 		var url = process.env.CLEARDB_DATABASE_URL;
+    console.log(url);
 		var grupos = url.match(/mysql:\/\/(.*):(.*)@(.*)\/(.*)\?/);
 		return mysql.createConnection({
-			host:grupos[3],
-			user:grupos[1],
-			password:grupos[2],
-			database:grupos[4]
+			host:'us-cdbr-iron-east-03.cleardb.net',
+			user:'b47a2aab68dcf6',
+			password:'64ce4623',
+			database:'heroku_8a96551247824ef'
 		});
 	}
 }
